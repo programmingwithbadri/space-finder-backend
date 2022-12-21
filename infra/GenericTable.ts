61e802813e9008a586008f8dc9ac021d6d949017
Badri <programmingwithbadri@outlook.com>
2022-12-21 13:34:27 +0530

    Create dynamo table

---
+++ infra/GenericTable.ts
@@ -0,0 +1,29 @@
+import { Stack } from 'aws-cdk-lib'
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb'
+
+export class GenericTable {
+    private name: string
+    private primaryKey: string
+    private stack: Stack
+
+    constructor(name: string, primaryKey: string, stack: Stack) {
+        this.name = name
+        this.primaryKey = primaryKey
+        this.stack = stack
+        this.initialize()
+    }
+
+    private initialize() {
+        this.createTable()
+    }
+
+    private createTable() {
+        new Table(this.stack, this.name, {
+            partitionKey: {
+                name: this.primaryKey,
+                type: AttributeType.STRING,
+            },
+            tableName: this.name,
+        })
+    }
+}
