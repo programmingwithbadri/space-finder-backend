0e3ecc3a83f5d5c577c5a8398936c8d1d873a429
Badri <programmingwithbadri@outlook.com>
2022-12-21 13:12:38 +0530

    Create helloworld lambda

--- infra/SpaceStack.ts
+++ infra/SpaceStack.ts
@@ -1,8 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
+import {
+    Code,
+    Function as LambdaFunction,
+    Runtime,
+} from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs'
-
+import { join } from 'path'
 export class SpaceStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
+
+        const helloLambda = new LambdaFunction(this, 'helloLambda', {
+            runtime: Runtime.NODEJS_16_X,
+            code: Code.fromAsset(join(__dirname, '..', 'services', 'lambda')),
+            handler: 'hello.main',
+        })
     }
 }
