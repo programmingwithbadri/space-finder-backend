0f22e7adbabcf487018c4e0d321f155b261f2b82
Badri <programmingwithbadri@outlook.com>
2022-12-22 12:47:44 +0530

    small missing fix

--- infra/GenericTable.ts
+++ infra/GenericTable.ts
@@ -36,6 +36,7 @@ export class GenericTable {
 
     private initialize() {
         this.createTable();
+        this.addSecondaryIndexes();
         this.createLambdas();
         this.grantTableAccess();
     }
